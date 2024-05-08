20df8a5e385887d5d78af5c889d6d505318bc1d0
Luminat3 <bskin132@gmail.com>
2024-05-08 10:43:42 +0700

    perbaikan tampilan

--- resources/views/dashboard/history.blade.php
+++ resources/views/dashboard/history.blade.php
@@ -8,9 +8,6 @@
 
 @section('content')
     <div class="card">
-        <div class="card-header">
-            <h3 class="card-title">List Pelanggan</h3>
-        </div>
                 <!-- /.card-header -->
         <div class="card-body">
             <table id="example2" class="table table-bordered table-hover">
